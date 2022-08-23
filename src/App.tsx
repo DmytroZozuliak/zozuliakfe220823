622cd74cee0bba116b1d68b308d939882e481da5
DmytroZozuliak <dmytro.zozuliak@gmail.com>
2022-08-23 10:11:20 +0300

    feat: logic + visualisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from 'react'
+import './App.css'
+import poster1 from './assets/cats.jpg'
+import poster2 from './assets/chubaka.jpg'
+import poster3 from './assets/dogs.jpg'
+import poster4 from './assets/dyno.jpg'
+import poster5 from './assets/planets.jpg'
+import { getRandomColor, shuffleArray } from './utils/fhelpers'
+
+const imagesArray = [poster1, poster2, poster3, poster4, poster5]
+const seconds = 52
 
 function App() {
+  const [images, setImages] = useState(imagesArray)
+  const [buttonColor, setButtonColor] = useState('white')
+
+  const changeColorsAndBG = useCallback(() => {
+    setImages((prev) => shuffleArray(prev))
+    setButtonColor(getRandomColor())
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      changeColorsAndBG()
+    }, seconds * 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [changeColorsAndBG])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header
+        style={{ backgroundImage: `url(${images[0]})` }}
+        className="App-header center background"
+      >
+        header
       </header>
+      <main className="App-main">
+        <aside style={{ backgroundImage: `url(${images[1]})` }} className="center background">
+          Left Panel
+        </aside>
+        <section style={{ backgroundImage: `url(${images[2]})` }} className="center background">
+          <p>Main Body</p>
+          <button
+            style={{ backgroundColor: buttonColor }}
+            className="button"
+            onClick={changeColorsAndBG}
+          >
+            change
+          </button>
+        </section>
+        <aside style={{ backgroundImage: `url(${images[3]})` }} className="center background">
+          Right Panel
+        </aside>
+      </main>
+      <footer
+        style={{ backgroundImage: `url(${images[4]})` }}
+        className="App-footer center background"
+      >
+        footer
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/utils/fhelpers.ts
@@ -0,0 +1,17 @@
+export function shuffleArray<T>(array: T[]): T[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array
+};
+
+export function getRandomColor(): string {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
